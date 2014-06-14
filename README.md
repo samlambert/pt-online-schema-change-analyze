946acd64f271a43987fbab3a9c6811ab11822868
Sam Lambert <sam.lambert@github.com>
2014-06-14 11:21:16 -0500

    Analyze before the table swap
    The plugin now checks for errors and kills the migration if the analyze fails.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 pt-online-schema-change-analyze
 ===============================
 
-Analyze a MySQL table after migrating it.
+Analyze a MySQL table before swapping it in place.
 
 In certain cases it is recommended not to allow MySQL to recalculate table statistics as it can cause a drift in query plans. More info [here](http://www.mysqlperformanceblog.com/2011/10/06/when-does-innodb-update-table-statistics-and-when-it-can-bite/) and [here](http://www.mysqlperformanceblog.com/2013/12/03/innodb_stats_on_metadata-slow-queries-information_schema/).
 
-This can cause a problems when using [pt-online-schema-change](http://www.percona.com/doc/percona-toolkit/2.2/pt-online-schema-change.html) as the statistics might not be correct on the new table. This plugin analyzes the table right after the swap is done which allows MySQL to recalculate the statistics.
+This can cause a problems when using [pt-online-schema-change](http://www.percona.com/doc/percona-toolkit/2.2/pt-online-schema-change.html) as the statistics might not be correct on the new table. This plugin analyzes the table right before the swap is done which allows MySQL to recalculate the statistics and avoids slow queries.
 
 To use the plugin just add:
 ```
--- pt-online-schema-change-analyze.pl
+++ pt-online-schema-change-analyze.pl
@@ -9,17 +9,38 @@ sub new {
 }
 
 sub init {
-   my ($self, %args) = @_;
-   $self->{orig_tbl} = $args{orig_tbl};
+    my ($self, %args) = @_;
 }
 
-# After swapping the tables at the end of the migration an ANALYZE TABLE will run.
-sub after_swap_tables {
+# This fuction just exists just to set new_tbl which is not passed to before_swap_tables
+sub after_create_new_table {
    my ($self, %args) = @_;
-   my $dbh      = $self->{aux_cxn}->dbh;
-   my $orig_tbl = $self->{orig_tbl};
-   print "Running: ANALYZE TABLE " . $orig_tbl->{name} . ".\n";
-   $dbh->do("ANALYZE TABLE " . $orig_tbl->{name});
-   return;
+   $self->{new_tbl} = $args{new_tbl};
+}
+
+sub before_swap_tables {
+   my ($self, %args) = @_;
+   my $dbh = $self->{aux_cxn}->dbh;
+   my $new_tbl = $self->{new_tbl};
+
+   print "Running: ANALYZE TABLE " . $new_tbl->{name} . ".\n";
+
+   my @analyzed;
+   # Because MySQL does not return an error if you try and analyze a table that does not
+   # exist this is a select so we can read the results
+   @analyzed = eval { $dbh->selectrow_array("ANALYZE TABLE " . $new_tbl->{name}) };
+   if ( $@ ) {
+      print "ANALYZE TABLE failed with error:\n$@\n";
+      exit;
+   } elsif (!@analyzed) {
+      print "ANALYZE TABLE failed with error:\n", $dbh->errstr, "\n";
+      exit;
+   } elsif (@analyzed[2] =~ /error/i ) {
+      print "ANALYZE TABLE failed with error:\n @analyzed[3]\n";
+      exit;
+   } else {
+      print "ANALYZE succeeded with output:\n @analyzed[3]\n";
+      return;
+   }
 }
 1;
